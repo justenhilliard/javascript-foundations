7c32638625b1ce1dacec4b76b4b9328d4e4b0278
Justen <justenkhilliard@gmail.com>
2026-06-11 12:59:22 -0400

    Added if/else statement to practice syntax

--- 02-conditions.js
+++ 02-conditions.js
@@ -1,3 +1,50 @@
+// 02-conditionals.js
+// Practicing JavaScript conditionals
+
 const age = 20;
 const hasStudentId = true;
 const score = 87;
+
+function checkVotingAge(personAge) {
+  if (personAge >= 18) {
+    return "You are old enough to vote.";
+  } else {
+    return "You are not old enough to vote.";
+  }
+}
+
+function checkStudentDiscount(studentStatus) {
+  if (studentStatus) {
+    return "You qualify for the student discount.";
+  } else {
+    return "You do not qualify for the student discount.";
+  }
+}
+
+function checkGrade(testScore)
+{
+  if (testScore >= 90) 
+  {
+    return "Grade: A";
+  } 
+  else if (testScore >= 80) 
+  {
+    return "Grade: B";
+  } 
+  else if (testScore >= 70) 
+  {   
+    return "Grade: C";
+  } 
+  else if (testScore >= 60) 
+  {
+    return "Grade: D";
+  } 
+  else 
+  {
+    return "Grade: F";
+  }
+}
+
+console.log(checkVotingAge(age));
+console.log(checkStudentDiscount(hasStudentId));
+console.log(checkGrade(score));
